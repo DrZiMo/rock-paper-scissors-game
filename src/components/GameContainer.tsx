caae6dcec2a9d1e61639674028643c4cb28f1525
Dr_ZiMo <zuhaybhamar@gmail.com>
2025-06-22 21:19:42 +0300

    Fixed the height issue

--- src/components/GameContainer.tsx
+++ src/components/GameContainer.tsx
@@ -1,16 +1,19 @@
-import BattlePart from './BattlePart'
-import SelectPiece from './SelectPiece'
+import BattlePart from "./BattlePart";
+import SelectPiece from "./SelectPiece";
 
 const GameContainer = () => {
   return (
-    <div className='flex-1 flex justify-center items-center w-full max-w-[540px] h-full relative mx-auto outline outline-amber-500'>
+    <div
+      className="flex-1 flex justify-center items-center w-full max-w-[540px] h-full relative mx-auto"
+      style={{ paddingBlock: "70px" }}
+    >
       {/* 1. Select Piece */}
       <SelectPiece />
 
       {/* 2. The Battle */}
       <BattlePart />
     </div>
-  )
-}
+  );
+};
 
-export default GameContainer
+export default GameContainer;
--- src/components/SelectPiece.tsx
+++ src/components/SelectPiece.tsx
@@ -1,30 +1,30 @@
-import { useSelectedPiece } from '../store/SelectedPiece'
-import { AnimatePresence, motion } from 'framer-motion'
-import Piece from './Piece'
-import { useGameStatus } from '../store/GameState'
+import { useSelectedPiece } from "../store/SelectedPiece";
+import { AnimatePresence, motion } from "framer-motion";
+import Piece from "./Piece";
+import { useGameStatus } from "../store/GameState";
 
 const SelectPiece = () => {
-  const gameStatus = useGameStatus((state) => state.status)
-  const isSelected = useSelectedPiece((state) => state.selected)
+  const gameStatus = useGameStatus((state) => state.status);
+  const isSelected = useSelectedPiece((state) => state.selected);
 
   return (
-    <AnimatePresence mode='wait'>
+    <AnimatePresence mode="wait">
       {gameStatus && !isSelected && (
         <motion.div
-          key={'box'}
-          initial={{ left: '-100%' }}
-          animate={{ left: '0%' }}
-          exit={{ left: '-100%' }}
-          className='w-[65%] max-w-[340px] relative'
+          key={"box"}
+          initial={{ left: "-100%" }}
+          animate={{ left: "0%" }}
+          exit={{ left: "-100%" }}
+          className="w-[65%] max-w-[340px] relative"
         >
-          <img src='/images/bg-triangle.svg' className='mx-auto' />
-          <Piece piece='paper' position='left' />
-          <Piece piece='rock' position='bottom' />
-          <Piece piece='scissors' position='right' />
+          <img src="/images/bg-triangle.svg" className="mx-auto" />
+          <Piece piece="paper" position="left" />
+          <Piece piece="rock" position="bottom" />
+          <Piece piece="scissors" position="right" />
         </motion.div>
       )}
     </AnimatePresence>
-  )
-}
+  );
+};
 
-export default SelectPiece
+export default SelectPiece;
